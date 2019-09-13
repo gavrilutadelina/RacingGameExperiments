3d3ac9e9a513460c5e8ccc6ed24c3f9adcea1848
Adelina <gvr.adelina@yahoo.com>
2019-09-13 22:45:42 +0300

    set-up basic project structure

---
+++ src/main/java/com/fasttrackit/App.java
@@ -0,0 +1,13 @@
+package com.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
